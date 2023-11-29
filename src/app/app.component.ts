7b2d8788c3de9b2ab11821192e9c4ba7c6c1ae57
alainhortiz <alainhortiz@gmail.com>
2023-11-29 00:21:03 -0500

    Cambios hasta directivas de atributos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './componentes/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ButtonComponent, 
+    FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app28';
+  cities: string[] = ['Barcelona','Madrid','Malaga','Pamplona','Cádiz','Sevilla'];
+  
+  //declarar solamente una propiedad. fijenese que no tiene valor
+  name!: string;
+  pais!: string;
+  title1: string = 'Ejemplo de interpolación';
+  title2: string = 'Ejemplo de datos bidireccional';
+  title3: string = 'Ejemplo de directiva estructural';
+  title4: string = 'Ejemplo de directiva de atributos';
+  url: string = 'https://img1.g-star.com/product/c_fill,f_auto,h_675,q_80/v1685191895/D20147-D190-C744-M01W/g-star-raw-pantalones-pleated-chino-relaxed-verde.jpg';
 }
